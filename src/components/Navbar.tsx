60765d24ea262376169630bbd6834dd8574c775b
Prathama Biswas <146082822+prathama4@users.noreply.github.com>
2026-04-11 23:33:45 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       className="bg-white shadow-md px-6 py-4 flex justify-between items-center"
     >
       <h1 className="text-xl font-bold text-blue-600">
-        ChildCare Vaccinatiions
+        ChildCare Vaccination
       </h1>
 
       <Link
